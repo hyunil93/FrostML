ce4062db03840e8b7fd5cbfc37c9dceb2ba9d6db
winterchild <jooh@vtouch.io>
2022-06-16 14:19:47 +0900

    Update `EfficientFormer` classes
    * Add `EfficientFormer` class and its helper methods
    * Add `EfficientFormer` factory function
    * Add `EfficientFormer` L1, L3, L7 presets

---
+++ frostml/models/transformer/efficientformer/__init__.py
@@ -0,0 +1,4 @@
+from frostml.models.transformer.efficientformer.efficientformer import efficientformer_l1, efficientformer_l3, efficientformer_l7
+
+
+__all__ = ['efficientformer_l1', 'efficientformer_l3', 'efficientformer_l7']
