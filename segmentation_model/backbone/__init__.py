b7c7711c371eae605e21158f2464b98d8bf974ab
gusdlf93 <gusdlf93@naver.com>
2022-07-23 21:02:44 +0900

    change_main

--- segmentation_model/backbone/__init__.py
+++ segmentation_model/backbone/__init__.py
@@ -8,3 +8,26 @@ from segmentation_model.backbone import resnet_18
 from segmentation_model.backbone import resnet_50
 from segmentation_model.backbone import Wide_ResNet
 from segmentation_model.backbone import EfficientNet
+
+def load_model(model_name, num_classes=1000, img_size=704, pretrained=False):
+    models = {'EfficientNet' : EfficientNet.EfficientNet,
+              'ReResSegNet' : ReResSegNet.ReResSegNet,
+              'EfficientFormer' : EfficientFormer.efficientformer_l3,
+              'volo' : volo.volo_d1,
+              'MobileNet' : MobileNetV2.MobileNetV2,
+              'MAE_ViT' : MAE_ViT.MAE_VIT_B16,
+              'Wide_ResNet' : Wide_ResNet.Wide_Resnet,
+              'UNet' : unet.UNet,
+              'resnet_18' : resnet_18.ResNet18,
+              'resnet_50' : resnet_50.ResNet50
+              }
+
+    try:
+        if model_name == "EfficientNet":
+            model = models[model_name](num_classes=1000, img_size=704, pretrained=False)
+        else:
+            model = models[model_name](num_classes=1000, img_size=704)
+    except:
+        raise("Model load Error")
+
+    return model
